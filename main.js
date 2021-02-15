42721fdd6cb3f67363313be215754f59a4c8f8d9
Svante Pålsson <70195788+svante1987@users.noreply.github.com>
2021-02-15 22:35:11 +0100

    lagt till en del css styling

--- main.js
+++ main.js
@@ -11,10 +11,15 @@ button.addEventListener('click',function(){
 fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=6f8be410a1a8662918de11d14f4f922e')
 /* här är min promises, eller responses */
 .then(response => response.json())
+/* data är en funktion med variabler som vi kan få tag i */
 .then(data => {
   var nameValue = data['name'];
   var tempValue = data['main']['temp'];
   var descValue = data['weather'][0]['description'];
+/* inner HTML här för att javascript ska kunna visa informationen */
+  name.innerHTML = nameValue;
+  temp.innerHTML = tempValue;
+  desc.innerHTML = descValue;
 })
 
 
