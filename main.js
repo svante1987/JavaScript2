75ecb50a4c8b466e11cb047a64e48a0c09f661c4
Svante Pålsson <70195788+svante1987@users.noreply.github.com>
2021-02-15 23:26:42 +0100

    la till &units=metric' så att man ser celsius istället för fahrenheit

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var button= document.querySelector('.submit');
 /* klickar man på knappen körs fetch */
 button.addEventListener('click',function(name){
 /* här är min fetch i länken så ber jag fetch hämta användar input (+input.value+) */
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=6f8be410a1a8662918de11d14f4f922e')
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=6f8be410a1a8662918de11d14f4f922e&appid&units=metric')
 /* här är min promises, eller responses */
 .then(response => response.json())
 /* data är en funktion med variabler som vi kan få tag i */
