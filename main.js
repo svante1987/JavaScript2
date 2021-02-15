afb95168121336634809da380d7b704a19ce8fc9
Svante Pålsson <70195788+svante1987@users.noreply.github.com>
2021-02-15 22:20:55 +0100

    gjorde  data till en funktion

--- main.js
+++ main.js
@@ -5,12 +5,18 @@ var inputValue = document.querySelector('.name');
 var inputValue = document.querySelector('.desc');
 var inputValue = document.querySelector('.temp');
 
+/* klickar man på knappen körs fetch */
 button.addEventListener('click',function(){
 /* här är min fetch i länken så ber jag fetch hämta användar input (+input.value+) */
 fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=6f8be410a1a8662918de11d14f4f922e')
 /* här är min promises, eller responses */
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data => {
+  var nameValue = data['name'];
+  var tempValue = data['main']['temp'];
+  var descValue = data['weather'][0]['description'];
+})
+
 
 .catch(err => alert("Aj, aj den staden känner ingen till..."))
 })
