ffca0fc7aabd832965a0444ff4d5f72d19bd69ea
Svante Pålsson <70195788+svante1987@users.noreply.github.com>
2021-02-15 23:28:46 +0100

    la till mer info i kommentar om  weather featch api och kommenterade bort foursquare fetchen

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ var button= document.querySelector('.submit');
 
 /* klickar man på knappen körs fetch */
 button.addEventListener('click',function(name){
-/* här är min fetch i länken så ber jag fetch hämta användar input (+input.value+) */
+/* här är min fetch i länken så ber jag fetch hämta användar input 
+(+input.value+) för att se celsius la jag till (&units=metric) i slutet utav min key */
 fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=6f8be410a1a8662918de11d14f4f922e&appid&units=metric')
 /* här är min promises, eller responses */
 .then(response => response.json())
@@ -18,7 +19,7 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=6
   var nameValue = data['name'];
   var tempValue = data['main']['temp'];
   var descValue = data['weather'][0]['description'];
-/* inner HTML här för att javascript ska kunna visa informationen */
+/* inner HTML här för att javascript ska kunna visa informationen på webbplatsen */
 
   main.innerHTML = nameValue;
   temp.innerHTML = "Temp - "+tempValue;
@@ -35,8 +36,8 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=6
 /*variabler*/
 
 /*url med api key och api secret*/
-fetch('https://api.foursquare.com/v2/venues/search?ll='+input.value+
-'&client_id=ICQLB0Y105CEOG5MEUK0ESCCSFVVJVR5W3YZU01PWWNLN33Y&client_secret=WOPRSG1BSB0FWGZCRJCDZL2YRPKJTK53BH5TAZGZWNCS0MJS')
+/*fetch('https://api.foursquare.com/v2/venues/search?ll='+input.value+
+'&client_id=ICQLB0Y105CEOG5MEUK0ESCCSFVVJVR5W3YZU01PWWNLN33Y&client_secret=WOPRSG1BSB0FWGZCRJCDZL2YRPKJTK53BH5TAZGZWNCS0MJS')*/
 
 
 
