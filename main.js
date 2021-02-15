0c6031c6ba8ffc57976ad0817e33d5249f7bea7d
Svante Pålsson <70195788+svante1987@users.noreply.github.com>
2021-02-15 21:50:59 +0100

    lagt en fetch på openweather och skrivit in min api key

--- main.js
+++ main.js
@@ -4,3 +4,5 @@ var button = document.querySelector('.inputValue');
 var inputValue = document.querySelector('.name');
 var inputValue = document.querySelector('.desc');
 var inputValue = document.querySelector('.temp');
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=6f8be410a1a8662918de11d14f4f922e')
